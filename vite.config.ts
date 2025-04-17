277555fd1a4ac5d4bf78b3d8b0d12f7ce1b3808d
szludwikowski <sludwikowski.dev@gmail.com>
2025-04-17 08:49:59 +0200

    feat: Initial configuration added

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@context": path.resolve(__dirname, "src/context"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+});
